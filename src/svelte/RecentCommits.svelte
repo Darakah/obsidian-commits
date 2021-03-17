1186e1a94807044aa5ddcce0dfe1291bcee4b90d
Darakah <darakah.dark@gmail.com>
2021-03-18 00:29:03 +0100

    Version 0.1.1

--- src/svelte/RecentCommits.svelte
+++ src/svelte/RecentCommits.svelte
@@ -6,36 +6,36 @@
 </script>
 
 <b style="color: {fillColor}"> Created </b>
-{data["Created"]?.length} new note(s) <br />
+{data["Created"]?.filter(unique).length} new note(s) <br />
 <b style="color: {fillColor}"> Deleted </b>
-{data["Deleted"]?.length} note(s): {@html data["Deleted"]
+{data["Deleted"]?.filter(unique).length} note(s): {@html data["Deleted"]
     ?.filter(unique)
     .slice(0, top)} <br />
 <b style="color: {fillColor}"> Renamed </b>
-{data["Renamed"]?.length} note(s): {@html data["Renamed"]
+{data["Renamed"]?.filter(unique).length} note(s): {@html data["Renamed"]
     ?.filter(unique)
     .slice(0, top)} <br />
 <b style="color: {fillColor}"> Expanded </b>
-{data["Expanded"]?.length} note(s): {@html data["Expanded"]
+{data["Expanded"]?.filter(unique).length} note(s): {@html data["Expanded"]
     ?.filter(unique)
     .slice(0, top)} <br />
 <b style="color: {fillColor}"> Refactored </b>
-{data["Refactored"]?.length} note(s): {@html data["Refactored"]
+{data["Refactored"]?.filter(unique).length} note(s): {@html data["Refactored"]
     ?.filter(unique)
     .slice(0, top)} <br />
 <b style="color: {fillColor}"> Tagged </b>
-{data["Tagged"]?.length} note(s): {@html data["Tagged"]
+{data["Tagged"]?.filter(unique).length} note(s): {@html data["Tagged"]
     ?.filter(unique)
     .slice(0, top)} <br />
 <b style="color: {fillColor}"> Linked </b>
-{data["Linked"]?.length} note(s): {@html data["Linked"]
+{data["Linked"]?.filter(unique).length} note(s): {@html data["Linked"]
     ?.filter(unique)
     .slice(0, top)} <br />
 <b style="color: {fillColor}"> Removed Tags from </b>
-{data["Removed Tags from"]?.length} note(s): {@html data["Removed Tags from"]
+{data["Removed Tags from"]?.filter(unique).length} note(s): {@html data["Removed Tags from"]
     ?.filter(unique)
     .slice(0, top)} <br />
 <b style="color: {fillColor}"> Removed Links from </b>
-{data["Removed Links from"]?.length} note(s): {@html data["Removed Links from"]
+{data["Removed Links from"]?.filter(unique).length} note(s): {@html data["Removed Links from"]
     ?.filter(unique)
     .slice(0, top)} <br />
